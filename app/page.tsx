49e7a31d7a4d00bcc9d2753e72152537d5176371
ZaidRamirez <zramirezh1801@alumno.ipn.mx>
2025-07-24 16:53:35 -0600

    Agegar tabla con mock user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,24 @@
-import { PostList } from '@/components/posts-list';
 import { createClient } from '@/utils/supabase/server';
-import { redirect } from "next/navigation";
+import { redirect } from 'next/navigation';
 import { PostsListData } from '@/types/posts';
-import { ComposePost } from '@/components/compose-post-server';
+import TableUI from '../components/tableUI/TableUI';
 
 export default async function App() {
-  const supabase = await createClient()
-  const { data: { user } } = await supabase.auth.getUser()
-  
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   if (user === null) {
-    redirect('/login')
+    redirect('/login');
   }
 
   const { data: posts }: { data: PostsListData } = await supabase.from('posts').select('*, users(*)').order('created_at', { ascending: false });
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between'>
-      <section className='max-w-[800px] w-full mx-auto border-l border-r border-white/30 min-h-screen'>
-        <ComposePost userAvatarUrl={user.user_metadata.avatar_url} />
-        <PostList posts={posts} />
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <section>
+        <TableUI />
       </section>
     </main>
-  )
+  );
 }
